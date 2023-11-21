c0da457cb126d0918cb87a51cd8a395418f91887
FacundoBisio <facubisio433@gmail.com>
2023-11-21 19:20:10 -0300

    .
    undefined

--- api/serializers.py
+++ api/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework.serializars import ModelSerializer
+from rest_framework.serializers import ModelSerializer
 from rest_framework import serializers
 from .models import Customers, Suppliers, Categories, Products, Orders, Orderdetails, Employees
 
